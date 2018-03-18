f5258368f8e833b6165ca9f122cc0f880e4f8726
Loan Laux <contact@loanlaux.fr>
2018-03-18 13:01:45 +0100

    Add ability to unsave from chat

--- imports/ui/components/LinkBubble.jsx
+++ imports/ui/components/LinkBubble.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import Truncate from "react-truncate";
 import styled from "styled-components";
+import classnames from "classnames";
 
 import { BubbleWrapper, Bubble } from "./styled";
 
@@ -30,26 +31,105 @@ const Description = styled.span`
   margin: 0 .6rem;
 `;
 
-const LinkBubble = ({ link, onLongPress, onClickStop }) => (
-  <BubbleWrapper sender={"anorak"}>
-    <LinkWrapper href={"#"} onClick={() => window.open(link.link, "_system")}>
-      <Bubble
-        sender={"me"}
-        suggestion
-        link
-      >
-        <Thumbnail src={link.image} />
-        <Title>
-          <Truncate lines={2} ellipsis={<span>...</span>}>
-            {link.title}
-          </Truncate>
-        </Title>
-        <Description>
-          {link.community}
-        </Description>
-      </Bubble>
-    </LinkWrapper>
-  </BubbleWrapper>
-);
+const LinkActions = styled.div`
+  display: flex;
+  flex-direction: column;
+  
+  i {
+    cursor: pointer;
+    margin-bottom: .5rem;
+    font-size: 1.3rem;
+    
+    &.active {
+      color: #0F90D1;
+    }
+  }
+`;
+
+class LinkBubble extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      liked: false,
+      disliked: false,
+      saved: props.link.isSavedForLater
+    };
+  }
+
+  handleLike = () => {
+    this.setState({ liked: !this.state.liked }, () => {
+      if (this.state.liked && this.state.disliked) {
+        this.setState({ disliked: false });
+      }
+    });
+  };
+
+  handleDislike = () => {
+    this.setState({ disliked: !this.state.disliked }, () => {
+      if (this.state.disliked && this.state.liked) {
+        this.setState({ liked: false });
+      }
+    });
+  };
+
+  handleSaveForLater = () => {
+    this.setState({ saved: !this.state.saved });
+
+    this.props.onSaveForLaterClick(this.props.link.id);
+  };
+
+  render() {
+    const { link } = this.props;
+
+    return (
+      <BubbleWrapper sender={"anorak"}>
+        <LinkWrapper href={"#"} onClick={() => window.open(link.link, "_system")}>
+          <Bubble
+            sender={"me"}
+            suggestion
+            link
+          >
+            <Thumbnail src={link.image} />
+            <Title>
+              <Truncate lines={2} ellipsis={<span>...</span>}>
+                {link.title}
+              </Truncate>
+            </Title>
+            <Description>
+              {link.community}
+            </Description>
+          </Bubble>
+        </LinkWrapper>
+        <LinkActions>
+          <i
+            className={classnames({
+              "fa": true,
+              "fa-thumbs-up": true,
+              "active": this.state.liked
+            })}
+            onClick={this.handleLike}
+          />
+          <i
+            className={classnames({
+              "fa": true,
+              "fa-thumbs-down": true,
+              "active": this.state.disliked
+            })}
+            onClick={this.handleDislike}
+          />
+          <i
+            className={classnames({
+              "fa": true,
+              "fa-bookmark": true,
+              "active": this.state.saved
+            })}
+            onClick={this.handleSaveForLater}
+          />
+        </LinkActions>
+      </BubbleWrapper>
+    );
+  }
+}
 
 export default LinkBubble;
