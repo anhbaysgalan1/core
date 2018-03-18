b338cf12898ee3dc90b2eb9892ba14f9ac737cc5
Loan Laux <contact@loanlaux.fr>
2018-03-18 11:19:48 +0100

    Remove react-click-n-hold

--- imports/ui/components/LinkBubble.jsx
+++ imports/ui/components/LinkBubble.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import Truncate from "react-truncate";
 import styled from "styled-components";
 
-import ClickAndHold from "react-click-n-hold";
-
 import { BubbleWrapper, Bubble } from "./styled";
 
 const LinkWrapper = styled.a`
@@ -34,25 +32,23 @@ const Description = styled.span`
 
 const LinkBubble = ({ link, onLongPress, onClickStop }) => (
   <BubbleWrapper sender={"anorak"}>
-    <ClickAndHold onEnd={onClickStop} onClickNHold={onLongPress} time={1}>
-      <LinkWrapper href={"#"} onClick={() => window.open(link.link, "_system")}>
-        <Bubble
-          sender={"me"}
-          suggestion
-          link
-        >
-          <Thumbnail src={link.image} />
-          <Title>
-            <Truncate lines={2} ellipsis={<span>...</span>}>
-              {link.title}
-            </Truncate>
-          </Title>
-          <Description>
-            {link.community}
-          </Description>
-        </Bubble>
-      </LinkWrapper>
-    </ClickAndHold>
+    <LinkWrapper href={"#"} onClick={() => window.open(link.link, "_system")}>
+      <Bubble
+        sender={"me"}
+        suggestion
+        link
+      >
+        <Thumbnail src={link.image} />
+        <Title>
+          <Truncate lines={2} ellipsis={<span>...</span>}>
+            {link.title}
+          </Truncate>
+        </Title>
+        <Description>
+          {link.community}
+        </Description>
+      </Bubble>
+    </LinkWrapper>
   </BubbleWrapper>
 );
 
