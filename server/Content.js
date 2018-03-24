4afa7014f1f368916f827f8c3b3e3fab6b8fa45b
Loan Laux <contact@loanlaux.fr>
2018-03-24 17:58:06 +0100

    Fetch data from SP

--- server/Content.js
+++ server/Content.js
@@ -4,22 +4,63 @@ Meteor.methods({
   "content/getRandomFromCategory": async (category, skill) => {
     const mysql = await import("promise-mysql");
 
-    const connection = await mysql.createConnection(Meteor.settings.mysql);
+    console.log(`content/getRandomFromCategory ${category} ${skill}`);
 
-    const query = mysql.format(`
-      SELECT *
-      FROM cd_raw_intake 
-      WHERE material_type LIKE ?
-      AND categories LIKE ?
-      ORDER BY RAND()
-      LIMIT 3
-    `, [`%${category}%`, `%${skill}%`]);
+    let connection;
+    let categoryId;
+    let skillId;
 
-    const results = await connection.query(query);
+    const categoryTypeQuery = mysql.format(`
+      SELECT row_id
+      FROM cd_type
+      WHERE name LIKE ?
+    `, [`%${category}%`]);
 
-    connection.end();
+    const skillTypeQuery = mysql.format(`
+      SELECT row_id
+      FROM cd_category
+      WHERE name LIKE ?
+    `, [`%${skill}%`]);
 
-    return results;
+    return mysql.createConnection(Meteor.settings.mysql)
+      .then((connectionObject) => connection = connectionObject)
+      .then(() => connection.query(skillTypeQuery))
+      .then((skillRow) => {
+        if (skillRow[0] && skillRow[0].row_id) {
+          console.log("Skill type rows", skillRow[0].row_id);
+
+          skillId = skillRow[0].row_id;
+        } else {
+          throw new Meteor.Error("skill-not-found", `Couldn't find skill matching ${skill}`);
+        }
+      })
+      .then(() => connection.query(categoryTypeQuery))
+      .then((categoryRow) => {
+        if (categoryRow[0] && categoryRow[0].row_id) {
+          console.log("Category type row", categoryRow[0].row_id);
+
+          categoryId = categoryRow[0].row_id;
+        } else {
+          throw new Meteor.Error("category-not-found", `Couldn't find category matching ${category}`);
+        }
+      })
+      .then(() => {
+        console.log("skillId", skillId);
+        console.log("categoryId", categoryId);
+
+        const query = mysql.format(`
+          CALL get_content_by_typeANDcat(?,?);
+        `, [categoryId, skillId]);
+
+        return connection.query(query);
+      })
+      .then((results) => {
+        console.log("getRandomFromCategory results", results[0]);
+
+        connection.end();
+
+        return results[0]; // Because of raw MySQL data structure. Return empty array if undefined.
+      });
   },
 
   "content/report": async (link) => {
